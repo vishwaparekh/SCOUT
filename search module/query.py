71f14e9c1aed3762e984e7aeab9c2a3c2be7fcb5
s-dharmam <savanidharmam5@gmail.com>
2021-11-04 11:03:44 +0530

    fixes

--- search module/query.py
+++ search module/query.py
@@ -2,5 +2,5 @@ from extract import Extract
 from search import Search
 
 #a=Extract("pdf_name")
-search = Search("a.npy")
-search.call_query("dallas new york arizona /// ^^^")
+search = Search("pdf_name.npy")
+search.call_query("enter query here ")
